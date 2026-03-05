c78bf1ed19706dcc78bfcc411cf3f819931b5537
Toby2507 <oluwatobisalau2000@gmail.com>
2026-03-05 19:15:21 +0100

    chore: rename worker-intelligence to intelligence-worker

--- apps/intelligence-worker/src/main.ts
+++ apps/intelligence-worker/src/main.ts
@@ -3,13 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const logger = new Logger('WorkerIntelligence');
+  const logger = new Logger('Intelligence Worker');
   const app = await NestFactory.createApplicationContext(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
 
   app.enableShutdownHooks();
-  logger.log('Worker intelligence started');
+  logger.log('Intelligence worker started');
 }
 
 bootstrap();

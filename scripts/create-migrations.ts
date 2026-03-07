6abfe5f686d08754585a456bbf4f3f8d1152942f
Toby2507 <oluwatobisalau2000@gmail.com>
2026-03-07 12:30:03 +0100

    refactor(database): update the migration template in the generation script

--- scripts/create-migrations.ts
+++ scripts/create-migrations.ts
@@ -13,11 +13,23 @@ const MIGRATIONS_DIR = path.resolve(__dirname, '../libs/database/src/migrations'
 const MIGRATION_TEMPLATE = `import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  
+  await sql
+    .raw(
+      \`
+
+      \`,
+    )
+    .execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  
+  await sql
+    .raw(
+      \`
+
+      \`,
+    )
+    .execute(db);
 }
 `;
 

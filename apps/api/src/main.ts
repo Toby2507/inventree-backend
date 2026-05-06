a98695f4bd2aebb40909b26e6786b75998a6a08f
Toby2507 <oluwatobisalau2000@gmail.com>
2026-05-06 19:38:36 +0100

    feat(security): add http security to the api entry file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,37 @@
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import compression from 'compression';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('bootstrap');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(helmet());
+  app.use(compression());
+  app.enableCors({ origin: '*', credentials: true });
+  app.enableVersioning({ type: VersioningType.URI });
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3000;
+  const apiUrl = configService.get('API_URL', `http://localhost:${port}`);
+  await app.listen(port);
+
+  logger.log(
+    `\n\n🚀 Inventree Backend is up and running!\n🌍 Environment: ${configService.get('NODE_ENV')}\n📊 Health Check: ${apiUrl}/api/health\n📚 API Documentation: ${apiUrl}/api/docs`,
+  );
 }
 bootstrap();

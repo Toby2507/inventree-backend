3e91b77c7a4e7ff85860a8bc983ee4865eec5782
Toby2507 <oluwatobisalau2000@gmail.com>
2026-05-09 21:03:47 +0100

    feat(identity/presentation): add the identity module, auth controller, and user dtos

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,14 @@
+import { DomainExceptionFilter } from '@app/common';
 import { validate } from '@app/config';
+import { GeneratorModule } from '@app/core';
 import { DatabaseModule } from '@app/database';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { CqrsModule } from '@nestjs/cqrs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IdentityModule } from './identity';
 
 @Module({
   imports: [
@@ -12,9 +16,11 @@ import { AppService } from './app.service';
     CqrsModule.forRoot(),
     // Globals
     DatabaseModule,
+    GeneratorModule,
     // Modules
+    IdentityModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_FILTER, useClass: DomainExceptionFilter }],
 })
 export class AppModule {}

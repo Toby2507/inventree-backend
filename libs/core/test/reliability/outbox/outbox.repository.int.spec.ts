5940e50bf21d30d260accc99f24c509eebce9190
Toby2507 <oluwatobisalau2000@gmail.com>
2026-06-14 07:36:52 +0100

    test(core): add integration test for the outbox repository

---
+++ libs/core/test/reliability/outbox/outbox.repository.int.spec.ts
@@ -0,0 +1,52 @@
+import { OutboxKyselyRepository } from '@app/core/reliability/outbox/persistence/outbox.kysely.repository';
+import { OperationalDB, OperationalSchema } from '@app/database';
+import { faker } from '@app/testing';
+import { fsSerializedOutboxContext } from '@app/testing/core/observability';
+import { feOutboxEvent } from '@app/testing/core/reliability/outbox';
+import { createTestContext, TestContext } from '@app/testing/database';
+
+describe('OutboxKyselyRepository (integration)', () => {
+  let ctx: TestContext<OperationalSchema>;
+  let db: OperationalDB;
+  let repo: OutboxKyselyRepository;
+
+  beforeAll(async () => {
+    ctx = await createTestContext();
+    repo = new OutboxKyselyRepository();
+  });
+  beforeEach(async () => {
+    db = await ctx.begin();
+  });
+  afterEach(async () => {
+    await ctx.rollback();
+  });
+  afterAll(async () => {
+    await ctx.dispose();
+  });
+
+  describe('insert', () => {
+    it('should insert events into the outbox table', async () => {
+      const record = {
+        events: feOutboxEvent.generateMany(1),
+        ctx: {
+          serialized: fsSerializedOutboxContext.generate({ actorStoreId: undefined }),
+          traceId: faker.string.uuid(),
+          spanId: faker.string.uuid(),
+        },
+      };
+      await repo.insert(db, record);
+      const rows = await db.selectFrom('outbox_events').selectAll().execute();
+      expect(rows).toHaveLength(1);
+      const [row] = rows;
+      const [event] = record.events;
+      expect(row.payload).toEqual(
+        expect.objectContaining({
+          ...event.payload,
+          _obs: expect.objectContaining({ correlationId: record.ctx.serialized.correlationId }),
+        }),
+      );
+      expect(row.trace_id).toBe(record.ctx.traceId);
+      expect(row.correlation_id).toBe(record.ctx.serialized.correlationId);
+    });
+  });
+});
